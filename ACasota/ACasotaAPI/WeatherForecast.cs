5642157054e05f0c703224738a55d4faebdaf4de
Andre <pires.19989@gmail.com>
2023-01-18 23:33:24 +0000

    HALLO

--- ACasota/ACasotaAPI/WeatherForecast.cs
+++ ACasota/ACasotaAPI/WeatherForecast.cs
@@ -9,5 +9,8 @@ namespace ACasotaAPI
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string? HALLO { get; set; }
+
     }
 }
