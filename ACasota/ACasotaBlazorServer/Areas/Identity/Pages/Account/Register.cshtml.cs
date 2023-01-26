03423f8e2b6957bc9c52d510cc83a13125ab0253
Daniel <daniel.1899.ferreira@gmail.com>
2023-01-26 18:54:41 +0000

    add backend code Register, Login, Logout

--- ACasota/ACasotaBlazorServer/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ACasota/ACasotaBlazorServer/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,61 @@
+using ACasotaBlazorServer.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace ACasotaBlazorServer.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+
+        public InputModel Input { get; set; }   
+
+        public string ReturnUrl { get; set; }
+
         public void OnGet()
         {
+            ReturnUrl = Url.Content("~/");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ReturnUrl = Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var identity = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var result = await _userManager.CreateAsync(identity, Input.Password);
+                
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(identity, isPersistent: false);
+                    return LocalRedirect(ReturnUrl);
+                }
+            }
+
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
         }
     }
 }
