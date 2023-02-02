fcea41ddfc650c3ec32c23eb19fa6e8c3df8ac4a
Daniel <daniel.1899.ferreira@gmail.com>
2023-02-02 15:47:49 +0000

    Commit final changes

--- ACasota/ACasotaBlazorServer/Data/DataContext.cs
+++ ACasota/ACasotaBlazorServer/Data/DataContext.cs
@@ -8,7 +8,6 @@ namespace ACasotaBlazorServer.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-            
         }
 
 		protected override void OnModelCreating(ModelBuilder builder)
