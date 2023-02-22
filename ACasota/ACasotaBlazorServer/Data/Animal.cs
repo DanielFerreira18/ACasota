13a6525991613b612f3b6f9f2a425e9ae30cd524
Daniel <daniel.1899.ferreira@gmail.com>
2023-02-22 10:55:20 +0000

    Rethought on the AnimalUser Connection New Migration

--- ACasota/ACasotaBlazorServer/Data/Animal.cs
+++ ACasota/ACasotaBlazorServer/Data/Animal.cs
@@ -5,10 +5,14 @@ namespace ACasotaBlazorServer.Data
 {
     public class Animal
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Race { get; set; }
         public string Age { get; set; }
         public string Colour { get; set; }
+
+        //FK to the owner of the animal
+        public string? UserId { get; set; }
+        public ApplicationUser User { get; set; }
     }
 }
